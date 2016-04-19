4a78895781b3ac6a04f9d29b9f2f6c439afaddea
Minh Dinh <dtminhpt@gmail.com>
2016-04-19 23:24:40 +0700

    Building a JSON API with Rails – Part 4: Implementing Authentication - Phase 1: The Initial Request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
-class ApplicationController < ActionController::API
+
+  class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
   def default_serializer_options
     { root: false }
+  end 
+
+  def token
+    authenticate_with_http_basic do |email, password|
+      user = User.find_by(email: email)
+      if user && user.password == password
+        render json: { token: user.auth_token }
+      else
+        render json: { error: 'Incorrect credentials' }, status: 401 
+      end 
+    end
   end 
 end
